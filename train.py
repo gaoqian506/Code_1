9df76f3a353dff6b4bbcae0452e9b1c333a4bb3e
gaoqian506 <gaoqian506@163.com>
2020-12-17 13:22:09 +0800

    准备替换成原始的krrish94的代码

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm, trange
 from code_1.train_utils import run_one_iter_of_nerf
 from code_1.nerf_helpers import get_ray_bundle, meshgrid_xy, mse2psnr, img2mse
+from code_1.load_blender import load_blender_data
+from code_1.load_llff import load_llff_data
 import time
 import torchvision
 
@@ -48,43 +50,43 @@ def main():
         )
         USE_CACHED_DATASET = True
     else:
-    # Load dataset
-    images, poses, render_poses, hwf = None, None, None, None
-    if cfg.dataset.type.lower() == "blender":
-        images, poses, render_poses, hwf, i_split = load_blender_data(
-            cfg.dataset.basedir,
-            half_res=cfg.dataset.half_res,
-            testskip=cfg.dataset.testskip,
-        )
-        i_train, i_val, i_test = i_split
-        H, W, focal = hwf
-        H, W = int(H), int(W)
-        hwf = [H, W, focal]
-        if cfg.nerf.train.white_background:
-            images = images[..., :3] * images[..., -1:] + (1.0 - images[..., -1:])
-    elif cfg.dataset.type.lower() == "llff":
-        images, poses, bds, render_poses, i_test = load_llff_data(
-            cfg.dataset.basedir, factor=cfg.dataset.downsample_factor
-        )
-        hwf = poses[0, :3, -1]
-        poses = poses[:, :3, :4]
-        if not isinstance(i_test, list):
-            i_test = [i_test]
-        if cfg.dataset.llffhold > 0:
-            i_test = np.arange(images.shape[0])[:: cfg.dataset.llffhold]
-        i_val = i_test
-        i_train = np.array(
-            [
-                i
-                for i in np.arange(images.shape[0])
-                if (i not in i_test and i not in i_val)
-            ]
-        )
-        H, W, focal = hwf
-        H, W = int(H), int(W)
-        hwf = [H, W, focal]
-        images = torch.from_numpy(images)
-        poses = torch.from_numpy(poses)
+        # Load dataset
+        images, poses, render_poses, hwf = None, None, None, None
+        if cfg.dataset.type.lower() == "blender":
+            images, poses, render_poses, hwf, i_split = load_blender_data(
+                cfg.dataset.basedir,
+                half_res=cfg.dataset.half_res,
+                testskip=cfg.dataset.testskip,
+            )
+            i_train, i_val, i_test = i_split
+            H, W, focal = hwf
+            H, W = int(H), int(W)
+            hwf = [H, W, focal]
+            if cfg.nerf.train.white_background:
+                images = images[..., :3] * images[..., -1:] + (1.0 - images[..., -1:])
+        elif cfg.dataset.type.lower() == "llff":
+            images, poses, bds, render_poses, i_test = load_llff_data(
+                cfg.dataset.basedir, factor=cfg.dataset.downsample_factor
+            )
+            hwf = poses[0, :3, -1]
+            poses = poses[:, :3, :4]
+            if not isinstance(i_test, list):
+                i_test = [i_test]
+            if cfg.dataset.llffhold > 0:
+                i_test = np.arange(images.shape[0])[:: cfg.dataset.llffhold]
+            i_val = i_test
+            i_train = np.array(
+                [
+                    i
+                    for i in np.arange(images.shape[0])
+                    if (i not in i_test and i not in i_val)
+                ]
+            )
+            H, W, focal = hwf
+            H, W = int(H), int(W)
+            hwf = [H, W, focal]
+            images = torch.from_numpy(images)
+            poses = torch.from_numpy(poses)
 
     # Seed experiment for repeatability
     seed = cfg.experiment.randomseed
